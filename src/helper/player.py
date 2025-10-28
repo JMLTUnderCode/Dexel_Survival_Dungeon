864d10dadf2867c8887c3a85648ca84b6b265ead
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-10-28 12:43:39 -0400

    Ajuste de posicion de personaje.

--- src/helper/player.py
+++ src/helper/player.py
@@ -3,7 +3,7 @@ from configs.package import CONF
 
 def create_player(
     type="oldman",
-    position=(CONF.MAIN_WIN.RENDER_TILE_SIZE*30, CONF.MAIN_WIN.RENDER_TILE_SIZE*30),
+    position=(CONF.MAIN_WIN.RENDER_TILE_SIZE*25, CONF.MAIN_WIN.RENDER_TILE_SIZE*30),
     collider_box=(CONF.PLAYER.COLLIDER_BOX_WIDTH, CONF.PLAYER.COLLIDER_BOX_HEIGHT),
     max_speed=250,
 ):
