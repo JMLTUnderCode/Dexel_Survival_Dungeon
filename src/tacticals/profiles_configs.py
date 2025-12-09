6263952bea0358cd041e5e00be0cd01e00c1131a
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-12-09 15:31:17 -0400

    Ajuste de parámetros tácticos para comportamientos en general.

--- src/tacticals/profiles_configs.py
+++ src/tacticals/profiles_configs.py
@@ -9,22 +9,22 @@ from configs.package import CONF
 HUNTER_COMBAT_PROFILE = TacticalProfile(weights={
     CONF.TACTICAL.TYPES.NARROW: -100.0,   # Medio Negativo (Prefiere: batalla en embudos)
     CONF.TACTICAL.TYPES.EXPOSED: -200.0,  # Gran Negativo (Prefiere mucho: busca zonas abiertas)
-    CONF.TACTICAL.TYPES.SENTRY: 50.0,     # Pequeño Positivo (Evita: no le gustan las paredes)
-    CONF.TACTICAL.TYPES.COVER: 0.0        # Indiferente
+    CONF.TACTICAL.TYPES.SENTRY: 100.0,    # Medio Positivo (Evita: no le gustan las paredes)
+    CONF.TACTICAL.TYPES.COVER: 200.0      # Gran Positivo (Evita mucho: no le gustan las esquinas)
 })
 
 # PERFIL GUARDIAN (Defensor)
 GUARDIAN_COMBAT_PROFILE = TacticalProfile(weights={
-    CONF.TACTICAL.TYPES.NARROW: 50.0,     # Pequeño Positivo (Evita levemente embudo)
+    CONF.TACTICAL.TYPES.NARROW: 50.0,     # Pequeño Positivo (Evita levemente: batalla en embudos)
     CONF.TACTICAL.TYPES.EXPOSED: 200.0,   # Gran Positivo (Evita fuertemente zonas abiertas)
     CONF.TACTICAL.TYPES.SENTRY: -200.0,   # Gran Negativo (Prefiere mucho: posición cubierta)
-    CONF.TACTICAL.TYPES.COVER: 0.0        # Indiferente
+    CONF.TACTICAL.TYPES.COVER: -100.0     # Medio Negativo (Prefiere: moverse por las esquinas)
 })
 
 # PERFIL DE HUIDA (Tactical Flee)
 FLEE_PROFILE = TacticalProfile(weights={
-    CONF.TACTICAL.TYPES.NARROW: -100.0,   # Medio Negativo (Prefiere embudos para cortar visión)
-    CONF.TACTICAL.TYPES.EXPOSED: 200.0,   # Gran Positivo (Evitar fuertemente zonas abiertas)
-    CONF.TACTICAL.TYPES.SENTRY: -100.0,   # Medio Negativo (Prefiere posición cubierta)
-    CONF.TACTICAL.TYPES.COVER: -200.0     # Gran Negativo (Prefiere moverse por las esquinas)
+    CONF.TACTICAL.TYPES.NARROW: 200.0,   # Medio Positivo (Evita mucho: se acorralado en embudos)
+    CONF.TACTICAL.TYPES.EXPOSED: 200.0,   # Gran Positivo (Evita mucho: zonas abiertas)
+    CONF.TACTICAL.TYPES.SENTRY: -200.0,   # Gran Negativo (Prefiere mucho: posición cubierta)
+    CONF.TACTICAL.TYPES.COVER: -200.0     # Gran Negativo (Prefiere mucho: moverse por las esquinas)
 })
